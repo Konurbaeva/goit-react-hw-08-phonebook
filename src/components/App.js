e1dbc21fa2b9e1305ee2a53f9e29278eec4ae8f9
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-11-17 07:29:28 +0100

    fixed BrowserRouter basename

--- src/components/App.js
+++ src/components/App.js
@@ -25,25 +25,46 @@ export const App = () => {
     'Fetching user data...'
   ) : (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute component={RegisterPage} redirectTo="/contacts" />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={<PrivateRoute component={Contacts} redirectTo="/login" />}
-        />
-      </Route>
-    </Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index exact element={<HomePage />} />
+      <Route
+        path="/register"
+        element={
+          <RestrictedRoute element={<RegisterPage/>} redirectTo="/contacts" />
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <RestrictedRoute element={<LoginPage/>} redirectTo="/contacts" />
+        }
+      />
+      <Route
+        path="/contacts"
+        element={<PrivateRoute element={<Contacts/>} redirectTo="/login" />}
+      />
+    </Route>
+  </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index exact element={<HomePage />} />
+    //     <Route
+    //       path="/register"
+    //       element={
+    //         <RestrictedRoute component={RegisterPage} redirectTo="/contacts" />
+    //       }
+    //     />
+    //     <Route
+    //       path="/login"
+    //       element={
+    //         <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
+    //       }
+    //     />
+    //     <Route
+    //       path="/contacts"
+    //       element={<PrivateRoute component={Contacts} redirectTo="/login" />}
+    //     />
+    //   </Route>
+    // </Routes>
   );
 };
