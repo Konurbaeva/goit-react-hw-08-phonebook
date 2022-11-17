cc993bbe72dbe2ce45e6f278c4535c51dd64c06f
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-11-17 12:55:35 +0100

    removed unused

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -8,90 +8,9 @@ import css from './ContactForm.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// export const ContactForm = () => {
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     const text = form.elements.text.value;
-//     // TODO Madina use toastify?
-//     // if (text === '') {
-//     //   return toast.error('Please enter contact');
-//     // }
-
-//     dispatch(addContact(text));
-//     form.reset();
-//   };
-
-//   return (
-//     <form className={css.form} onSubmit={handleSubmit}>
-//       <input name="text" className={css.input} />
-//       <button type="submit" className={css.button}>
-//         Add contact
-//       </button>
-//     </form>
-//   );
-// };
-
-// id, name, phone
-// export const ContactForm = () => {
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     // TODO Madina use toastify?
-//     // if (text === '') {
-//     //   return toast.error('Please enter contact');
-//     // }
-
-//     // dispatch(addContact({name: form.elements.name.value, phone: form.elements.phone.value}));
-//     dispatch(addContact({
-//       name: form.elements.name.value,
-//       phone: form.elements.phone.value
-//     }));
-//     form.reset();
-//   };
-
-//   return (
-//     <form className={css.form} onSubmit={handleSubmit}>
-//       <input name="text" className={css.input} />
-//       <button type="submit" className={css.button}>
-//         Add contact
-//       </button>
-//     </form>
-//   );
-// };
-
 export function ContactForm() {
   const getContacts = useSelector(getFilteredContacts)
    const dispatch = useDispatch();
- 
-  //  const handleSubmit = e => {
-  //    e.preventDefault();
-  //    const form = e.target;
-  //    const contactExists = getContacts.find(item => item.name === form.elements.name.value)
- 
-  //    if(contactExists) { 
-  //      toast.warn('🦄 Contact exists!', {
-  //        position: "top-right",
-  //        autoClose: 5000,
-  //        hideProgressBar: false,
-  //        closeOnClick: true,
-  //        pauseOnHover: true,
-  //        draggable: true,
-  //        progress: undefined,
-  //        theme: "light",
-  //        });
- 
-  //      return
-  //    }
- 
-  //    const action = addContact({name:form.elements.name.value, number:form.elements.number.value})
-  //    dispatch(action)
-  //    form.reset();
-  //  };
 
   const handleSubmit = e => {
     e.preventDefault();
